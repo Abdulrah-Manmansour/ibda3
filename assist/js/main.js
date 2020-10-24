d390daa4292865484cb3cd0d8501fd8045f49edd
abdo <abdulrahmanmansour600@gmail.com>
2020-10-24 16:53:31 +0200

    first commit

---
+++ assist/js/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    $(".siv-carousel").owlCarousel({
+        loop:true,
+        rtl:true,
+        nav:true,
+    margin:10,
+    autoplay:true,
+    autoplaySpeed:1000,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            margin:-40
+        },
+        600:{
+            items:2,
+            nav:true,
+            margin:-20
+        },
+        1000:{
+            items:3,
+            nav:true,
+            loop:false
+        }
+    }
+
+
+    });
+    $(".client-casroul").owlCarousel({
+        loop:true,
+        rtl:true,
+        nav:true,
+    
+    autoplay:true,
+    autoplaySpeed:1000,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:2,
+            nav:false,
+            margin:90
+        },
+        600:{
+            items:2,
+            nav:true,
+            margin:-20
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:false
+        }
+    }
+
+
+    });
+    $('.navbar-toggler ,.owl-prev ,.owl-next ').click(function() {
+        this.blur();
+      });
+  });
---
+++ assist/js/slider_project.js
@@ -0,0 +1,34 @@
+
+
+    $num = $('.my-card').length;
+    $even = $num / 2;
+    $odd = ($num + 1) / 2;
+    
+    if ($num % 2 == 0) {
+      $('.my-card:nth-child(' + $even + ')').addClass('activ');
+      $('.my-card:nth-child(' + $even + ')').prev().addClass('prev');
+      $('.my-card:nth-child(' + $even + ')').next().addClass('next');
+    } else {
+      $('.my-card:nth-child(' + $odd + ')').addClass('activ');
+      $('.my-card:nth-child(' + $odd + ')').prev().addClass('prev');
+      $('.my-card:nth-child(' + $odd + ')').next().addClass('next');
+    }
+    
+    $('.my-card').click(function() {
+      $slide = $('.activ').width();
+      console.log($('.activ').position().right);
+      
+      if ($(this).hasClass('next')) {
+        $('.card-carousel').stop(false, true).animate({right: '-=' + $slide});
+      } else if ($(this).hasClass('prev')) {
+        $('.card-carousel').stop(false, true).animate({right: '+=' + $slide});
+      }
+      
+      $(this).removeClass('prev next');
+      $(this).siblings().removeClass('prev activ next');
+      $(this).addClass('activ');
+      $(this).prev().addClass('prev');
+      $(this).next().addClass('next');
+    });
+    
+    
